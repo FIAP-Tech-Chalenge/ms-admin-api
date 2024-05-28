8fad9438435f1cfa304ace89d0d3697c94032bed
Lucas Rigon <lucas@bussolasocial.com.br>
2024-05-27 22:17:45 -0300

    first commit

---
+++ src/test/java/com/fiap/msadminapi/MsAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.msadminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAdminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
