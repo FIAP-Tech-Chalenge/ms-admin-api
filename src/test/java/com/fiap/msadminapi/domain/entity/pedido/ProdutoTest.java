1d99c42856bd020e2a618e6f471b1ed8baa956c2
Jonathan Oliveira <jonathansroliveira@gmail.com>
2024-06-28 09:44:23 -0300

    Adicionando testes

--- src/test/java/com/fiap/msadminapi/domain/entity/pedido/ProdutoTest.java
+++ src/test/java/com/fiap/msadminapi/domain/entity/pedido/ProdutoTest.java
@@ -14,11 +14,20 @@ public class ProdutoTest {
         var uuid = UUID.randomUUID();
         var quantidade = 1;
         var categoria = CategoriaEnum.BEBIDA;
+        var valor = Float.parseFloat("17.9");
         var produto = new Produto(uuid, quantidade, categoria);
+        produto.setValor(valor);
+        produto.setCategoria(CategoriaEnum.LANCHE);
 
         assertThat(produto.getUuid()).isEqualTo(uuid);
         assertThat(produto.getQuantidade()).isEqualTo(quantidade);
-        assertThat(produto.getCategoria()).isEqualTo(categoria);
+        assertThat(produto.getCategoria()).isEqualTo(CategoriaEnum.LANCHE);
+        assertThat(produto.getValor()).isEqualTo(valor);
+        assertThat(produto.toString()).isEqualTo("Produto(uuid=" + uuid + ", quantidade=" + quantidade + ", valor=" + valor + ", categoria=" + CategoriaEnum.LANCHE + ")");
+        assertThat(produto.getUuid().equals(uuid)).isTrue();
+        assertThat(produto.getCategoria().equals(CategoriaEnum.LANCHE)).isTrue();
+        assertThat(produto.getQuantidade().equals(quantidade)).isTrue();
+        assertThat(produto.hashCode()).isNotZero();
     }
 
 }
